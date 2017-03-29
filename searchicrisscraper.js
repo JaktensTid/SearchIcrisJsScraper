9ac038372c6bc5f4418cac0dff2741e25a937d29
JaktensTid <jaktenstid1@gmail.com>
2017-03-29 10:26:27 +0300

    To firefox

--- searchicrisscraper.js
+++ searchicrisscraper.js
@@ -4,12 +4,12 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://searchicris.co.weld.co.us/recorder/eagleweb/*
+// @match        https://searchicris.co.weld.co.us/recorder/eagleweb/docSearchResults.jsp?searchId=*
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
-
-document.onload(function () {
+debugger;
+document.onload = function () {
     var currentUrl = window.location.href;
     if (currentUrl.contains('docSearchResults.jsp')) {
         var arr = [];
@@ -29,13 +29,14 @@ document.onload(function () {
         var iframe = document.createElement("iframe");
         document.getElementsByClassName('warning')[0].appendChild(iframe);
         for(var i = 0; i < arr.length; i++){
+            var href = arr[i].
             iframeDoc = iframe.contentWindow.document;
-            iframeDoc.onload(function () {
-                
-            })
+            iframeDoc.onload = function (doc) {
+                var record = ScrapeRecord(document);
+            };
         }
     }
-});
+};
 
 function NextPage() {
 
