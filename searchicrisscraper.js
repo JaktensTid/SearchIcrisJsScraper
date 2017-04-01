49e057d0336ed376f2ec4427e435480fe2a5217c
jaktenstid <jaktenstid1@gmail.com>
2017-04-01 23:10:20 +0300

    Final

--- searchicrisscraper.js
+++ searchicrisscraper.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         SearchIcrisScraper
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      1.0
+// @description  Scraper for searchicris.co.weld.co.us
+// @author       IB
 // @match        https://searchicris.co.weld.co.us/recorder/eagleweb/docSearchResults.jsp?searchId=*
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
