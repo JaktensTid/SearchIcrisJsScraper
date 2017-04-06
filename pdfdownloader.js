62b4864781962731ba7e758c81611e75daeaf03a
JaktensTid <jaktenstid1@gmail.com>
2017-04-06 23:40:49 +0300

    pdfdownloader ended

--- pdfdownloader.js
+++ pdfdownloader.js
@@ -8,7 +8,12 @@
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.8.2/jquery.csv.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jszip-utils/0.0.2/jszip-utils.min.js
 // ==/UserScript==
+var zip = new JSZip();
+var currentRecord = 0;
 
 $(document).ready(function() {
     let form = document.getElementsByName('docSearch');
@@ -16,25 +21,52 @@ $(document).ready(function() {
     fileSelect = $(fileSelect);
     fileSelect.attr('type', 'file');
     $(form).prepend(fileSelect);
-    fileSelect.onclick = function() {this.value = null;};
+    fileSelect.onclick = function() {
+        this.value = null;
+    };
     fileSelect[0].onchange = function(e) {
-    	var files = e.target.files;
-        var file = files[0];           
+        var files = e.target.files;
+        var file = files[0];
         var reader = new FileReader();
         reader.onload = function(evt) {
             var csv = $.csv.toObjects(evt.target.result);
+            AddToZip(csv);
         };
         reader.readAsText(file);
+
     };
 });
 
-function Download(csv)
-{
-	let pattern = 
-	for(let i = 0; i < csv.length; i++)
-	{
-		let href = csv[i]['href'];
-		let reception = csv[i]['RECEPTION NO'];
-		let str = 'https://searchicris.co.weld.co.us/recorder/eagleweb/downloads/${reception}?id=${href}.A0&parent=${href}preview=false&noredirect=true'
-	}
+// Recur adding to zip
+function AddToZip(csv) {
+    let href = csv[currentRecord]['href'].split('=')[1];
+    let reception = csv[currentRecord]['RECEPTION NO'];
+    let urlToPdf = 'https://searchicris.co.weld.co.us/recorder/eagleweb/downloads/' + reception + '?id=' + href + '.A0&parent=' + href + '&preview=false&noredirect=true';
+    let name = 'Rec - ' + reception + '_url - ' + href + '.pdf';
+    sleep(5000);
+    JSZipUtils.getBinaryContent(urlToPdf, function(err, data) {
+        if (err) {
+            throw err; // or handle the error
+        }
+        if (currentRecord == 3) {
+            zip.generateAsync({
+                    type: "blob"
+                })
+                .then(function(content) {
+                    // see FileSaver.js
+                    saveAs(content, "Instruments.zip");
+                });
+        } else {
+            zip.file(name, data, {
+                binary: true
+            });
+            console.log('Fetching ' + currentRecord);
+            currentRecord++;
+            AddToZip(csv);
+        }
+    });
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
